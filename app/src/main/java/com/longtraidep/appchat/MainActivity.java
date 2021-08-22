e0ded8aeeac64f51c338a1d44f66e6065d187977
nbnlong2712 <nhatlonghn1203@gmail.com>
2021-08-22 17:29:34 +0700

    Long-feat: save state and logout feature

--- app/src/main/java/com/longtraidep/appchat/MainActivity.java
+++ app/src/main/java/com/longtraidep/appchat/MainActivity.java
@@ -1,15 +1,95 @@
 package com.longtraidep.appchat;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TableLayout;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.ExtendedFloatingActionButton;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.longtraidep.appchat.User.Users;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    FirebaseUser mFirebaseUser;
+    DatabaseReference mDatabaseReference;
+    private ViewPager2 mViewPager;
+    private TabLayout mTabLayout;
+
+    private ExtendedFloatingActionButton mFBtnUser;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        mFirebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        mDatabaseReference = FirebaseDatabase.getInstance().getReference("Users").child(mFirebaseUser.getUid());
+
+        mDatabaseReference.addValueEventListener(new ValueEventListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Users user = snapshot.getValue(Users.class);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+            }
+        });
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(this);
+        mViewPager.setAdapter(viewPagerAdapter);
+        new TabLayoutMediator(mTabLayout, mViewPager, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                switch (position)
+                {
+                    case 0:
+                        tab.setText(R.string.chat);
+                        break;
+                    case 1:
+                        tab.setText(R.string.user);
+                        break;
+                }
+            }
+        }).attach();
+    }
+
+    public void init()
+    {
+        mFBtnUser = (ExtendedFloatingActionButton) findViewById(R.id.fbtn_user);
+        mViewPager = (ViewPager2) findViewById(R.id.vpg_main);
+        mTabLayout = (TabLayout) findViewById(R.id.tablayout_main);
+
+        mFBtnUser.setOnClickListener(this);
+    }
+
+    @SuppressLint("NonConstantResourceId")
+    @Override
+    public void onClick(View v) {
+        switch (v.getId())
+        {
+            case R.id.fbtn_user:
+                startActivity(new Intent(MainActivity.this, UserActivity.class));
+                break;
+        }
     }
 }
---
+++ app/src/main/java/com/longtraidep/appchat/ViewPagerAdapter.java
@@ -0,0 +1,38 @@
+package com.longtraidep.appchat;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+import com.longtraidep.appchat.Fragment.ChatFragment;
+import com.longtraidep.appchat.Fragment.UserFragment;
+
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position)
+        {
+            case 0:
+                return new ChatFragment();
+            case 1:
+                return new UserFragment();
+            default:
+                return new ChatFragment();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 2;
+    }
+}
